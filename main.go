f60bfc26f0a7215b8d74480d7a89848ffc68a504
jb <jb@jb-Aspire-5750G.(none)>
2015-09-21 16:28:05 +0200

    problems with the dtd, need to check this

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 type temp struct {
-	X int
-	Y int
+	X string
+	Y string
 }
 
 //compile all templates and cache them
@@ -16,22 +16,43 @@ var templates = template.Must(template.ParseGlob("views/*"))
 
 func main() {
 	http.HandleFunc("/public/", visualHandler)
-	http.HandleFunc("/", hello)
-
+	http.HandleFunc("/", index)
+    
+    http.HandleFunc("/newip", formHandler)    
+    
 	fmt.Println("This server is going up on port 8080")
 	http.ListenAndServe(":8080", nil)
 
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+// index page
+func index(res http.ResponseWriter, req *http.Request) {
     fmt.Println("im in hello")
-	s := temp{1, 2}
+	s := temp{"JULLE", "DDANI"}
 	templates.ExecuteTemplate(res, "index", s)
 
 }
 
-func visualHandler(res http.ResponseWriter, req *http.Request) {
-    fmt.Println("some1 asked for ", req.URL.Path[1:])	
+//function for css and js
+func visualHandler(res http.ResponseWriter, req *http.Request) {	
     http.ServeFile(res, req, req.URL.Path[1:])
     
 }
+
+//function for handling the html form
+func formHandler(res http.ResponseWriter, req *http.Request) {
+    fmt.Println("some1 asked for formhandler ")	
+    ip := req.PostFormValue("ip")
+    fmt.Println(ip)
+    //do some cool stuffs
+    //check if ip is in db
+    //some db logic stuffs
+    s := temp{ip, ip}
+    templates.ExecuteTemplate(res, "index", s) //render a page
+    
+    
+}
+
+
+
+
---
+++ xslt-fake.xsl
@@ -0,0 +1,34 @@
+<?xml version="1.0"?>
+<xsl:stylesheet version="1.0"
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns="http://www.w3.org/1999/xhtml">
+
+    <xsl:template match="information">
+    <html>
+        <head>
+            <title>ServerMonitor</title>
+        </head>
+
+        <body>
+            <xsl:apply-templates select="CPU"/>
+        </body>
+
+    </html>
+    </xsl:template>
+   
+
+
+    <xsl:template match="CPU">
+        <h2>
+            <xsl:apply-templates select="temp"/>
+        </h2>
+    </xsl:template>
+
+    <xsl:template match="temp">
+        <h2>
+            <xsl:value-of select="."></xsl:value-of>
+            <xsl:value-of select="@unit"></xsl:value-of>
+        </h2>
+    </xsl:template>
+
+</xsl:stylesheet>
