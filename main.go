16daf3bf2869cc6f0a47b4b742963108da57d148
jb <jb@jb-Aspire-5750G.(none)>
2015-09-21 14:45:57 +0200

    css example is finito

--- main.go
+++ main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "html/template"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
-
 type temp struct {
-    X int
-    Y int
+	X int
+	Y int
 }
 
 //compile all templates and cache them
 var templates = template.Must(template.ParseGlob("views/*"))
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("This server is going up on port 8080")
-    http.ListenAndServe(":8080", nil)
-    
+	http.HandleFunc("/public/", visualHandler)
+	http.HandleFunc("/", hello)
+
+	fmt.Println("This server is going up on port 8080")
+	http.ListenAndServe(":8080", nil)
+
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    s := temp{1,2}
-    templates.ExecuteTemplate(res, "index", s)
-    
+    fmt.Println("im in hello")
+	s := temp{1, 2}
+	templates.ExecuteTemplate(res, "index", s)
+
 }
 
-
+func visualHandler(res http.ResponseWriter, req *http.Request) {
+    fmt.Println("some1 asked for ", req.URL.Path[1:])	
+    http.ServeFile(res, req, req.URL.Path[1:])
+    
+}
